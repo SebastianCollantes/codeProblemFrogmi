c812f756c5be243ea5831dd34022e01b96305105
SebastianCollantes <sebastian.collantes@outlook.es>
2023-08-02 23:01:13 -0400

    se agregaron comentarios en el código

--- problem-test.ts
+++ problem-test.ts
@@ -1,3 +1,4 @@
+// Esta interfaz la hice para tipar los datos de los incidentes
 interface Incident {
     descripcion: string;
     estado: string;
@@ -6,6 +7,8 @@ interface Incident {
 }
 
 class Store {
+    // desarrollé un arreglo en bruto para no ir metiendo cada dato,
+    // se pueden modificar estos datos de cualquier manera, era para ahorrar tiempo
     incidentsCollection: Incident[] = [
         {
             descripcion: 'El piso de la oficina está sucio con café',
@@ -39,13 +42,17 @@ class Store {
         }
     ];
 
+    // Esta es la función que recibe el rango de fechas
     incident_status(startDate: Date, endDate: Date) {
+        // filtramos los incidentes en: Abiertos - Solucionados
         const openedCases = this.incidentsCollection.filter(openedIncident => openedIncident.estado == 'Abierto');
         const closedCases = this.incidentsCollection.filter(openedIncident => openedIncident.estado == 'Solucionado');
 
+        // acá filtramos los incidentes abiertos entre las fechas de entrada
         const incidentesAbiertosEntreFechas = openedCases.filter(incidente => {
             return incidente.fecha_incidente >= startDate && incidente.fecha_incidente <= endDate;
         })
+        // acá lo mismo, filtramos los incidentes solucionados entre las fechas
         const incidentesSolucionadosEntreFechas = closedCases.filter(incidente => {
             return incidente.fecha_incidente >= startDate && incidente.fecha_incidente <= endDate;
         })
@@ -55,9 +62,13 @@ class Store {
         }
     }
 }
+// Ejecutamos la clase Store
 const newStore = new Store()
+// Estas variables son las que se pueden modificar para verificar de mejor manera la funcionalidad del programa
 const startRange = new Date('02-08-2023');
 const endRange = new Date('05-08-2023');
+// acá tomamos las fechas y llamamos a la función incident_status con las fechas asignadas manualmente
 const incidentsDateRange = newStore.incident_status(startRange, endRange);
 
+// imprimimos el resultado en consola
 console.log('Resultado:', incidentsDateRange)
